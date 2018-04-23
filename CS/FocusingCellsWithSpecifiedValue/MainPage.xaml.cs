ccb284e29158a4b440412160eaaca079dc50f2b6
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/FocusingCellsWithSpecifiedValue/MainPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace FocusingCellsWithSpecifiedValue {
+    public partial class MainPage : UserControl {
+        public MainPage() {
+            InitializeComponent();
+            grid.DataSource = new ProductList();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e) {
+            if (grid.VisibleRowCount == 0) return;
+            int rowHandle = grid.View.FocusedRowHandle;
+            if (grid.View.FocusedColumn.FieldName == "UnitPrice" && Convert.ToDouble(grid.GetFocusedValue()) == 21)
+                rowHandle++;
+            while (Convert.ToDouble(grid.GetCellValue(rowHandle, "UnitPrice")) != 21 &&
+                grid.IsValidRowHandle(rowHandle)) {
+                rowHandle++;
+            }
+            if (grid.IsValidRowHandle(rowHandle)) {
+                grid.View.FocusedColumn = grid.Columns["UnitPrice"];
+                grid.View.FocusedRowHandle = rowHandle;
+            }
+        }
+    }
+}
---
+++ VB/FocusingCellsWithSpecifiedValue/MainPage.xaml.vb
@@ -0,0 +1,31 @@
+﻿Imports Microsoft.VisualBasic
+Imports System
+Imports System.Windows
+Imports System.Windows.Controls
+
+Namespace FocusingCellsWithSpecifiedValue
+	Partial Public Class MainPage
+		Inherits UserControl
+		Public Sub New()
+			InitializeComponent()
+			grid.DataSource = New ProductList()
+		End Sub
+
+		Private Sub Button_Click(ByVal sender As Object, ByVal e As RoutedEventArgs)
+			If grid.VisibleRowCount = 0 Then
+				Return
+			End If
+			Dim rowHandle As Integer = grid.View.FocusedRowHandle
+			If grid.View.FocusedColumn.FieldName = "UnitPrice" AndAlso Convert.ToDouble(grid.GetFocusedValue()) = 21 Then
+				rowHandle += 1
+			End If
+			Do While Convert.ToDouble(grid.GetCellValue(rowHandle, "UnitPrice")) <> 21 AndAlso grid.IsValidRowHandle(rowHandle)
+				rowHandle += 1
+			Loop
+			If grid.IsValidRowHandle(rowHandle) Then
+				grid.View.FocusedColumn = grid.Columns("UnitPrice")
+				grid.View.FocusedRowHandle = rowHandle
+			End If
+		End Sub
+	End Class
+End Namespace
